d570f1741ae48264805a9eeae2c320f319ea6d69
Aaron Smyth <aas395@gmail.com>
2014-06-20 13:12:35 -0400

    adding requestanimationframe

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var requestAnimationFrame = (function(){
+	  return  window.requestAnimationFrame       ||
+	          window.webkitRequestAnimationFrame ||
+	          window.mozRequestAnimationFrame    ||
+	          function( callback ){
+	            return window.setTimeout(callback, 1000 / 60);
+	          };
+})();
+
 $(document).ready(function(){
 
 });
