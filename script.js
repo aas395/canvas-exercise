ed00470bfbf57c2aa4a68f70316899a50d011947
Aaron Smyth <aas395@gmail.com>
2014-06-20 10:29:18 -0400

    drawing initial circle

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-/*
- code goes here
-*/
+	
+	var canvas = document.getElementById('palette');
+	var ctx = canvas.getContext('2d');
+
+
+	//set canvas height and width
+	canvas.height = 200;
+	canvas.width = 200;
+
+	//draw a red circle
+	ctx.beginPath();
+	ctx.arc(100,100,50,0,2*Math.PI);
+	ctx.strokeStyle = '#000';
+	ctx.fillStyle = '#000';
+	ctx.stroke();
+	//ctx.fill();
+
 });
